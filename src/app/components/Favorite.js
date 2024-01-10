27dbc74e9d49376d3caa23b18edac8314bb5974c
Rishabh Singh <rishabh0739@gmail.com>
2024-01-10 15:16:12 +0530

    Fixes

--- src/app/components/Favorite.js
+++ src/app/components/Favorite.js
@@ -15,7 +15,7 @@ export default function Favorite({ favs, id, user }) {
         e.preventDefault();
 
         if (user) {
-            if (fav.some((favItem) => favItem.toString() === id)) {
+            if (fav.includes(Number(id))) {
                 setFav((prev) => prev.filter((favItem) => favItem.toString() !== id));
             } else {
                 setFav((prev) => [...prev, Number(id)]);
@@ -31,11 +31,10 @@ export default function Favorite({ favs, id, user }) {
             router.push('/login');
         }
     };
-
     return (
         <div className="relative flex justify-center items-center select-none w-12 cursor-pointer transition-all z-10">
             <span onClick={(e) => handleFavorite(e)} className="absolute right-0 flex justify-center items-center w-12 h-12 border-2 rounded-full bg-slate-50">
-                {fav.some((articleId) => articleId.toString() === id) ? (
+                {fav.includes(Number(id)) ? (
                     <IoMdHeart color="#2972f3" size={30} />
                 ) : (
                     <IoMdHeartEmpty color="#2972f3" size={30} />
